df0975255f83973958dbc780a1683e02b8315f20
Dong-Young Kim <31337.persona@gmail.com>
2022-06-23 01:26:04 +0900

    feat: implement `useRequest` module

--- src/hooks/useRequest.ts
+++ src/hooks/useRequest.ts
@@ -1,1 +1,17 @@
-export const useRequest = () => {};
+import { useQuery, UseQueryOptions, UseQueryResult } from "react-query";
+
+type Request = (...args: any[]) => any;
+
+type RequestOptions = UseQueryOptions;
+
+type Response<D = unknown, E = unknown> = UseQueryResult<D, E>;
+
+export const useRequest = (
+  request: Request,
+  { ...options }: RequestOptions,
+): Response =>
+  useQuery({
+    queryKey: request.name,
+    queryFn: request,
+    ...options,
+  });
